f44d698477a96dffbba932b3db584dd5d46d25a1
pwh-pwh <plmoknpwh@163.com>
2023-05-22 20:29:34 +0800

    add main.rs

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+mod error;
+mod model;
+mod web;
+
+pub use self::error::{Error, Result};
+use axum::extract::{Path, Query};
+use axum::middleware::map_response;
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::{get, get_service, Route};
+use axum::Router;
+use serde::Deserialize;
+use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+#[tokio::main]
+async fn main() {
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("server bind {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("main_response_mapper");
+    res
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handle_hello))
+        .route("/hello2/:name", get(handle_hello1))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+async fn handle_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    let name = params.name.as_deref().unwrap_or("world");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handle_hello1(Path(name): Path<String>) -> impl IntoResponse {
+    Html(format!("Hello <strong>{name}</strong>"))
+}
